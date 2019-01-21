3a6c1f56862da4b7302d4c8643ce68b7a88bcee6
Andrew Speck <15779151+ajspeck@users.noreply.github.com>
2019-01-20 20:48:28 -0500

    control replay visibility

--- website/js/now-racing-replay.js
+++ website/js/now-racing-replay.js
@@ -98,12 +98,17 @@ var Lineup = {
       $('#replay-video-src')[0].src = new_replay_url;
       this.replay_times=0;
       $('#replay-video')[0].load();
+      $('#replay-video').css('visibility','visible');
       $('#replay-video').off('ended');
       $('#replay-video').on('ended',$.proxy(function(){
         this.replay_times+=1;
         if (this.replay_times<this.replay_max_times)
         {
           $('#replay-video')[0].play();
+        }
+        else
+        {
+          $('#replay-video').css('visibility','hidden');
         }
       },this));
       $('#replay-video')[0].play();
